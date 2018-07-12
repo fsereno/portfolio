208de0980bc22f91d47552702e7004b830c5cbcf
Fabio Sereno <fabio_sereo@hotmail.com>
2018-07-12 22:53:54 +0100

    Building with zero html errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,54 @@
-'use strict';
-var gulp = require('gulp');
+"use strict";
+var gulp = require("gulp");
 var browserify = require("browserify");
-var source = require('vinyl-source-stream');
+var source = require("vinyl-source-stream");
 var tsify = require("tsify");
 var useref = require("gulp-useref");
-var sass = require('gulp-sass');
-var pug = require('gulp-pug');
-var gulpSequence = require('gulp-sequence')
+var sass = require("gulp-sass");
+var pug = require("gulp-pug");
+var gulpSequence = require("gulp-sequence")
 
 gulp.task("typeScript", function () {
   return browserify({
-      basedir: '.',
+      basedir: ".",
       debug: true,
-      entries: ['app/typeScript/application.ts'],
+      entries: ["app/typeScript/application.ts"],
       cache: {},
       packageCache: {}
   })
   .plugin(tsify)
   .bundle()
-  .pipe(source('application.js'))
+  .pipe(source("application.js"))
   .pipe(gulp.dest("app/js"));
 });
 
-gulp.task('sass', function () {
-  return gulp.src('app/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('app/css'));
+gulp.task("sass", function () {
+  return gulp.src("app/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("app/css"));
 });
 
-gulp.task('pug', function buildHTML() {
-  return gulp.src('app/pug/*.pug')
+gulp.task("pug", function buildHTML() {
+  return gulp.src("app/pug/**/*.pug")
   .pipe(pug({
     pretty: true
 
   }))
-  .pipe(gulp.dest('app'));
+  .pipe(gulp.dest("app"));
 });
 
-gulp.task('html',['typeScript', 'sass', 'pug'], function () {
-  return gulp.src('app/*.html')
+gulp.task("html",["typeScript", "sass", "pug"], function () {
+  return gulp.src("app/*.html")
     .pipe(useref())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest("dist"));
 });
 
-gulp.task('build', function(callback){
+gulp.task("build", function(callback){
 	gulpSequence(
-    ['typeScript', 'sass', 'pug', 'html'],
+    ["typeScript", "sass", "pug", "html"],
     callback);
 });
 
-gulp.task('default', function () {
-  gulp.start('build');
+gulp.task("default", function () {
+  gulp.start("build");
 });
