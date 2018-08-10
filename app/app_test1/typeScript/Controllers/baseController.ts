b5b84f16cc970a194fdd3563f2bf25cfa3729451
Fabio Sereno <fsereno@mutualvision.co.uk>
2018-08-10 09:32:59 +0100

    Added some new fields to the json object for active and to include in the home page links

---
+++ app/app_test1/typeScript/Controllers/baseController.ts
@@ -0,0 +1,38 @@
+// Interfaces
+import { ITextService } from "../../../typeScript/Interfaces/ITextService";
+
+// Models
+
+class BaseController  {
+    
+    textService: ITextService;
+
+    constructor
+    (
+
+        textService: ITextService
+      
+    ) 
+    {
+        this.textService = textService;
+    }
+
+    init(
+        
+    ) {
+
+        const self = this;
+
+        $(() => {
+
+            var text = self.textService.Concat("Application ", "Master Template");
+
+            console.log(text);
+
+        });
+
+    }
+       
+}
+
+export { BaseController };
