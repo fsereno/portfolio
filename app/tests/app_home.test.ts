7384ad69c365459f0f051e85ae9469c18d57c35b
Fabio Sereno <fabio_sereo@hotmail.com>
2018-09-26 22:33:50 +0100

    Started looking into implementing nightmare headless browser, is it the best one ?

---
+++ app/tests/app_home.test.ts
@@ -0,0 +1,10 @@
+//import { } from "nightmare";
+import { expect } from "chai";
+
+describe("app_home", () => {
+    describe("TestExample1", () => {
+        it("what ?", () => {
+            
+        });
+    });
+});
