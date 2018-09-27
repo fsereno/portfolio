9b8da51f33e9e18d9130f85e5a53ae9935781830
Fabio Sereno <fabio_sereo@hotmail.com>
2018-09-27 22:59:56 +0100

    Initial Nightmare test nearly working, it is not stopping the gulp process as it should, due to the async nature of promise

--- app/tests/app_home.test.ts
+++ app/tests/app_home.test.ts
@@ -1,10 +1,30 @@
-//import { } from "nightmare";
+import Nightmare  = require("nightmare");
 import { expect } from "chai";
 
 describe("app_home", () => {
-    describe("TestExample1", () => {
-        it("what ?", () => {
+    
+        var url = 'http://localhost:8080/app_dictionaryFinder/index.html';
+        
+        describe('Start page', () => {
+            it('should show form when loaded', () => {
+                new Nightmare({show:true})
+                    .goto(url)
+                    .type('form#inputForm input[name="input"]', "Sleep")
+                    .click('form#inputForm button[type="submit"]')
+                    
+                    .evaluate(() => {
+                        return jQuery("#result").text().length;
+                    })
+                    .then((result)=>{
+                        expect(result).to.be.equal(358);
+                    })
+                    .catch((error) => {
+                        console.log("ERROR")
+                    })
+                    
+            });
+
             
         });
-    });
+    
 });
