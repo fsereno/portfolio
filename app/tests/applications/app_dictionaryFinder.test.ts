c9698ef127a4822d1ac291a61301c7b9c3031a71
Fabio Sereno <fsereno@mutualvision.co.uk>
2018-10-03 17:26:03 +0100

    Working on using promises within tests

--- app/tests/applications/app_dictionaryFinder.test.ts
+++ app/tests/applications/app_dictionaryFinder.test.ts
@@ -1,5 +1,35 @@
+//let chai = require("chai");
+//let chaiAsPromised = require("chai-as-promised");
+//chai.use(chaiAsPromised)
+//let expect = chai.expect;
+
 import Nightmare  = require("nightmare");
-import { expect } from "chai";
+
+//let nightmare =  new Nightmare({show:true});
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinonChai = require('sinon-chai');
+
+const should = chai.should();
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+// Function to test
+const simple = {
+  test: async (input) => {
+    return input;
+  }
+}
+
+// Test
+describe('#Test', () => {
+  it('test', () => {
+    return simple.test(1).should.eventually.equal(1);
+  });
+});
+
+/*
 //https://wietse.loves.engineering/testing-promises-with-mocha-90df8b7d2e35
 describe("app_dictionaryFinder", () => {
     
@@ -7,6 +37,23 @@ describe("app_dictionaryFinder", () => {
         
         describe('Dictionary Finder', function(){
             it("Should return a string of length 358 when searching for 'Sleep'", function() {
+                
+                return expect(
+                    new Nightmare({show:true})
+                    .goto(url)
+                    .type('form#inputForm input[name="input"]', "Sleep")
+                    .click('form#inputForm button[type="submit"]')
+                    .evaluate(() => {
+                        return jQuery("#result").text().length;
+                    })
+                    .end()
+                
+                ).to.eventually.deep.equal(358);
+                
+                //.to.eventually.deep.equal(358)
+            });
+            
+            /*it("Should return a string of length 358 when searching for 'Sleep'", function() {
                 this.timeout(10000);
                 return new Nightmare({show:false})
                     .goto(url)
@@ -21,4 +68,4 @@ describe("app_dictionaryFinder", () => {
                     })
             });
         });
-});
+});*/
