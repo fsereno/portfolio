86bf8381114cb4105294b9a62dd807d0338797d0
Fabio Sereno <fabio_sereo@hotmail.com>
2018-10-08 22:36:56 +0100

    Initial template created for the example code

---
+++ app/C#/generics.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading;
+
+namespace Generics.Controller {
+
+	public class genericsController {
+		
+		public void ExampleMethod () 
+		{
+			Console.WriteLine("ExampleMethod...");
+		}
+	}	
+}
+
+namespace Generics.Programe {
+	
+	public class Programe {
+		
+		public void Main(){
+			
+			// code here
+		}
+	}
+}
