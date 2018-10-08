4f73baca8c0d27ccbcef1b35633f5e10f12b645c
Fabio Sereno <fabio_sereo@hotmail.com>
2018-10-08 21:35:03 +0100

    Events and Delegates example code in C#

---
+++ app/C#/eventsAndDelegates.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Threading;
+
+namespace EventsAndDelegates.HeatingSystem {
+
+	public class Instruction {
+		public DateTime StartTime;
+        public DateTime EndTime;
+        public int Degrees; 
+
+        public Instruction(
+            DateTime startTime,
+            DateTime endTime,
+            int degrees
+        ){
+            this.StartTime = startTime;
+            this.EndTime = endTime;
+            this.Degrees = degrees;
+        }
+	}
+
+	public class HeatingController {
+		
+		public delegate void HeatingControllerEngagedEventHandler(object source, EventArgs args);
+		public event HeatingControllerEngagedEventHandler HeatingEngaged;
+
+		public void Engage (Instruction instruction) 
+		{
+			Console.WriteLine("Engage Heating...");
+			Thread.Sleep(3000);
+			OnHeatingEngaged();
+		}
+
+		protected virtual void OnHeatingEngaged()
+		{
+			if(HeatingEngaged != null){
+				HeatingEngaged(this, EventArgs.Empty);
+			}
+		}
+	}	
+}
+
+namespace ProgrameNameSpace {
+	
+	public class Programe {
+		
+		public void Main(){
+			
+			var instruction = new EventsAndDelegates.HeatingSystem.Instruction(
+                new DateTime(),
+                new DateTime(),
+                21
+            );
+
+			var heatingController = new EventsAndDelegates.HeatingSystem.HeatingController(); // Publisher
+			var mailService = new MailService(); // Subscriber
+			var messageService = new MessageService(); // Subscriber
+
+			// Pointers / Subscribe to the Event
+			heatingController.HeatingEngaged += mailService.OnHeatingEngaged;
+			heatingController.HeatingEngaged += messageService.OnHeatingEngaged;
+
+			// Envoke the Encode method
+ 			heatingController.Engage(instruction);
+			Console.WriteLine("Process Complete, heating engaged!");
+		}
+	}
+
+	public class MailService 
+	{
+		public void OnHeatingEngaged(object source, EventArgs e)
+		{
+			// MailService  OnHeatingEngaged specific code here
+			System.Console.WriteLine("MailService sending an email...");
+		}
+	}
+
+	public class MessageService
+	{
+		public void OnHeatingEngaged(object source, EventArgs e)
+		{
+			// MessageService OnHeatingEngaged specific code here
+			System.Console.WriteLine("MessageService sending a text message...");
+		}
+	}
+}
