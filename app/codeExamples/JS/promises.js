2392119ba52156ed083ac734958d13c3d92b973a
Fabio Sereno <fabio_sereo@hotmail.com>
2018-10-09 21:32:13 +0100

    Example of promises completed

--- app/codeExamples/JS/promises.js
+++ app/codeExamples/JS/promises.js
@@ -1,1 +1,34 @@
 "use strict";
+
+// Change from true to false to trigger the catch error.
+var isApplictionSuccessful = true;
+
+// The Promise
+var canIAffordThisLoan = new Promise(
+    (resolve, reject) => {
+        if (isApplictionSuccessful) {
+            var decision = {
+                affordability: true,
+                creditCheck: true
+            };
+            resolve(decision);
+        } else {
+            var reason = new Error('You cannot afford this loan.');
+            reject(reason);
+        }
+    }
+);
+
+var askBank = () => {
+    canIAffordThisLoan
+        .then((fulfilled) => {
+            // If fulfilled then log out the decision.
+            console.log(fulfilled);
+        })
+        .catch((error) => {
+            // If not successful, then output the error.
+            console.log(error.message);
+        });
+}
+
+askBank();
