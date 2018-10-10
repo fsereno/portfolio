2bf36c7aa471dcf2df74bce3a29ca1278723358c
Fabio Sereno <fabio_sereo@hotmail.com>
2018-10-10 12:44:57 +0100

    Added an initial idea for findDuplicates, this is how initially this may be approached, followed by improved solution, with comments to explain

--- app/codeExamples/JS/findDuplicates.js
+++ app/codeExamples/JS/findDuplicates.js
@@ -2,7 +2,25 @@
 var arrayToUseNumeric = [2,7,5,1,7,4,6,3,4,6,8,7],
     arrayToUseAlpha = ["A","B","A","G","X","I","A","G","X","Z"];
 
-var findDuplicates = (array) => {
+// Initial solution, uses nested for loop. But this could be expensive with large amount of data.
+var findDuplicatesV1 = (array) => {
+
+    var resultArray = [],
+        duplicates = {};
+
+    for(var i = 0; i < array.length; i++){
+        for(var d = 0; d < array.length; d++){
+            if (d !== i && array[i] === array[d] && duplicates[array[i]] === undefined){
+                duplicates[array[i]] = true;
+                resultArray.push(array[i]);
+            }
+        }
+    }
+    return resultArray.join(",");
+}
+
+// Improved solution, removing the need for nested for loops.
+var findDuplicatesV2 = (array) => {
 
     var checked = {},
         duplicate = {},
@@ -21,5 +39,7 @@ var findDuplicates = (array) => {
     return resultArray.join(",");
 }
 
-console.log(findDuplicates(arrayToUseNumeric));
-console.log(findDuplicates(arrayToUseAlpha));
+console.log(findDuplicatesV1(arrayToUseNumeric));
+console.log(findDuplicatesV1(arrayToUseAlpha));
+console.log(findDuplicatesV2(arrayToUseNumeric));
+console.log(findDuplicatesV2(arrayToUseAlpha));
