169556eaa0db67f7dd3e468ae755268b4a03cf3a
Fabio Sereno <fabio_sereo@hotmail.com>
2018-10-10 22:55:29 +0100

    Recursion example added, shows how recursive methods work, remember them as nested function calls, with the result unwinding from each call, once the target is met

---
+++ app/codeExamples/JS/recursion.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// Basic Recursion Example
+var rec = (num) => {
+    //Fail Safe
+    if (num < 0){return};
+    // Target
+    if (num === 0){return 1};
+    // Recursion
+    return num + rec(num - 1);
+}
+
+console.log("Basic " + rec(1));
+
+// Complex Recursion Example
+function ChessboardTraveling(str) {
+  var   x = str[1], 
+        y = str[3],
+        a = str[6],
+        b = str[8];
+
+  var   xDiff = a - x, 
+        yDiff = b - y;
+  
+  return recur(xDiff, yDiff);
+  
+  function recur(xDiff, yDiff) {
+ 
+    if (xDiff === 0) { return 1; }
+    if (yDiff === 0) { return 1; }
+
+    var x = recur(xDiff - 1, yDiff),
+        y = recur(xDiff, yDiff - 1),
+        sum = x + y;
+
+    return sum;
+  }
+  
+}
+
+console.log("Complex " + ChessboardTraveling("(2 2)(4 3)"));
