8c0df973e5469f53f0b54b15f5b91b13412a34cd
Fabio Sereno <fabio_sereo@hotmail.com>
2018-10-15 21:43:51 +0100

    Updated unit tests to be more realistic

---
+++ app/app_dotnetCore/Services/StringService.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Services
+{
+    public class StringService : IStringService
+    {
+        public string ConcatStringArray(string[] stringArray)
+        {
+            var result = string.Empty;
+
+            foreach(var stringInput in stringArray)
+            {
+
+                result += stringInput + " ";
+
+            }
+
+            return result.Trim();
+        }
+    }
+}
