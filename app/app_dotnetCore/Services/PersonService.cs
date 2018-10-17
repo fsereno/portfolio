7bcfc948f73bbcd1da2a4a7f20ca8d69b153c456
Fabio Sereno <fabio_sereo@hotmail.com>
2018-10-17 17:24:33 +0100

    Starting to work on new service for new repository

---
+++ app/app_dotnetCore/Services/PersonService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Repositories;
+
+namespace Services
+{
+    public class PersonService : IPersonService
+    {
+       
+        readonly List<Person> _persons;
+        readonly IRepository<Person> _repository;
+
+        public PersonService(
+            IRepository<Person> repository)
+        {
+            _repository = repository;
+
+        }
+
+        public PersonService(List<Person> persons)
+        {
+            _persons = persons;
+
+        }
+
+        public void Add(Person person)
+        {
+
+            _repository.Add(person);
+
+        }
+
+        public Person Get(Person person)
+        {
+           var result = _repository.Get(person);
+            return result;
+        }
+    }
+}
