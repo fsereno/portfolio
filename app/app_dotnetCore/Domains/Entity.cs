1438d064912ae1bbcd4b333d78ecd36da0de7216
Fabio Sereno <fabio_sereo@hotmail.com>
2018-10-18 20:07:53 +0100

    Code tidy up

--- app/app_dotnetCore/Domains/Entity.cs
+++ app/app_dotnetCore/Domains/Entity.cs
@@ -5,7 +5,7 @@ namespace Domains
     public abstract class Entity : IComparable<Entity>
     {
         public int Id { get; set; }
-        public int Age {get;set;}
+        public int Age { get; set; }
 
         public int CompareTo(Entity other)
         {
