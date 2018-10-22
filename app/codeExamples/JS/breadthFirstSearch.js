bb6e833f2032dd93003b139748bb9e8b29fb4a27
Fabio Sereno <fabio_sereo@hotmail.com>
2018-10-22 13:14:56 +0100

    Breadth first search working

--- app/codeExamples/JS/breadthFirstSearch.js
+++ app/codeExamples/JS/breadthFirstSearch.js
@@ -10,60 +10,49 @@ class Node {
     }   
 }
 
-class Neighbours {
-    constructor(
-        u,d,l,r
-    ){
-        this.u = u;
-        this.d = d;
-        this.l = l;
-        this.r = r;
-    }
-}
-
-
-function findNeighbours(size,x,y){
-
-    let u = y-1 >= 1 ? y-1 : null;
-    let d = y+1 <= size ? y+1 : null;
-    let l = x-1 >= 1 ? x-1 : null;
-    let r = x+1 <= size ? x+1 : null;
-    var neighbours = [];
-
-    if(u!==null){
-        neighbours.push(new Node(u+","+x, x, u));
-    }
-    if(d!==null){
-        neighbours.push(new Node(d+","+x, x, d));
-    }
-    if(l!==null){
-        neighbours.push(new Node(y+","+l, l, y));
-    }
-    if(r!==null){
-        neighbours.push(new Node(y+","+r, r, y));
-    }
-
-    return neighbours;
-
-}
 class Grid {
     constructor(size)
     {
         this.nodes = [];
         this.size = size;
         this.dictionary = {};
+        this.start = null;
+        this.end = null;
         
         for (let n = 1; n <= this.size; n++) {
             for (let cell = 1; cell <= this.size; cell++) {
                 let nodeName = n+","+cell;
                 let node = new Node(nodeName, cell, n);
-                let neighbours = findNeighbours(this.size, cell, n);                
+                let neighbours = this.FindNeighbours(cell, n);                
                 node.neighbours = neighbours;
-
                 this.nodes.push(node);
                 this.dictionary[nodeName] = node;
             }
         }
+    }
+
+    FindNeighbours(x, y) {
+
+        let u = y-1 >= 1 ? y-1 : null;
+        let d = y+1 <= this.size ? y+1 : null;
+        let l = x-1 >= 1 ? x-1 : null;
+        let r = x+1 <= this.size ? x+1 : null;
+        var neighbours = [];
+
+        if(u!==null){
+            neighbours.push(u+","+x);
+        }
+        if(d!==null){
+            neighbours.push(d+","+x);
+        }
+        if(l!==null){
+            neighbours.push(y+","+l);
+        }
+        if(r!==null){
+            neighbours.push(y+","+r);
+        }
+
+        return neighbours;
     }
 
     AddStart(start){
@@ -78,60 +67,63 @@ class Grid {
         this.end = this.dictionary[end];
         return this.end;
     }
-}
-//Grid(4, "1,1", "3,2");
-var grid = new Grid(4);
-var queue = [];
 
-var start = grid.AddStart("1,1");
-var end = grid.AddEnd("3,2");
-//console.log("Start neighbour")
-//console.log(start.neighbours);
+    PrintPath(){
 
-grid.start.searched = true;
-queue.push(start);
+        var next = this.end;
+        var path = [];
+        path.push(this.end.name);
+        while(next.parent !== null){
+    
+            path.push(next.parent);
+            next = this.dictionary[next.parent];
+        }
+    
+        var text = "";
+        path.forEach(move => {
+            text += move + " --> ";
+        });
+    
+        return text;
+    
+    }
 
-while(queue.length > 0 ){
+    BreadthFirstSearch(){
 
-    var current = queue.shift();
+        var queue = [];
+        this.start.searched = true;
+        queue.push(this.start);
 
-    //console.log(current.name)
-    //console.log(end.name)
-    //console.log(queue.length);
+        while(queue.length > 0 ){
 
-    if(current.name === end.name) {
+            var current = queue.shift();
 
-        console.log("Found! " +current.name);
-        break;
-    } else {
+            if(current.name === end.name) {
 
-        var neighbours = current.neighbours;
+                console.log("Found! " + current.name);
+                break;
 
-        console.log(neighbours.length);
+            } else {
 
-        for (let i = 0; i < neighbours.length; i++) {
-            const neighbour = neighbours[i];
+                var neighbours = current.neighbours;
 
-            //console.log(neighbour.searched)
-            
-            if(!neighbour.searched){
-
-                neighbour.searched = true;
-                neighbour.parent = current;
-                queue.push(neighbour);
-
-                //console.log(neighbour)
-
+                for (let i = 0; i < neighbours.length; i++) {
+                    const neighbour = this.dictionary[neighbours[i]];
+                    
+                    if(!neighbour.searched){
+                        neighbour.searched = true;
+                        neighbour.parent = current.name;
+                        queue.push(neighbour);
+                    }
+                }
             }
         }
-
-
     }
 }
 
-grid.nodes.forEach(n => {
-
-    console.log(n);
-   // console.log("parent " + n.name);
-
-})
+//Programe
+let grid = new Grid(4);
+let start = grid.AddStart("1,2");
+let end = grid.AddEnd("3,2");
+grid.BreadthFirstSearch();
+console.log(grid.PrintPath());
