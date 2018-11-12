81d2096687413da0db18b14cd0a75c7fe6902f39
Fabio Sereno <fabio_sereo@hotmail.com>
2018-11-12 15:16:19 +0000

    Update now in place, may reconsider design, viewmodel is more complex than it needs to be

--- app/app_koTypeScript/typeScript/Models/PeopleViewModel.ts
+++ app/app_koTypeScript/typeScript/Models/PeopleViewModel.ts
@@ -3,17 +3,24 @@ export class PeopleViewModel {
     collection: KnockoutObservableArray<PeopleModel>;
     name: KnockoutObservable<string>;
     age: KnockoutObservable<number>;
-    delete: PeopleModel;
+    nameToEdit: KnockoutObservable<string>;
+    ageToEdit: KnockoutObservable<number>;
+    itemToDelete: PeopleModel;
+    currentItem: PeopleModel;
 
     constructor(name: string, age: number) {
         this.collection = ko.observableArray(new Array<PeopleModel>());
         this.name = ko.observable(name);
         this.age = ko.observable(age);
+        this.nameToEdit = ko.observable("");
+        this.ageToEdit = ko.observable(0);
+        this.currentItem = new PeopleModel("", 0);
         this.fetch();
     }   
 
     public add(){
-        this.collection.push(new PeopleModel(this.name(), this.age()))
+        if(this.name().length > 0 && this.age() > 0)
+            this.collection.push(new PeopleModel(this.name(), this.age()));
     }
 
     public fetch() : void {
@@ -22,10 +29,24 @@ export class PeopleViewModel {
     }
 
     public remove = (item: PeopleModel) => {
-        this.delete = item;
+        this.itemToDelete = item;
     }
 
     public removeConfirm = () => {
-        this.collection.remove(this.delete);
+        if(this.collection.indexOf(this.itemToDelete) > -1)
+            this.collection.remove(this.itemToDelete);
+    }
+
+    public edit = (item: PeopleModel) => {
+        this.currentItem = item;
+        this.nameToEdit(item.name);
+        this.ageToEdit(item.age);
+    }
+
+    public update = () => {
+        if(this.collection.indexOf(this.currentItem) > -1) {
+            var replace = new PeopleModel(this.nameToEdit(), this.ageToEdit())
+            this.collection.replace(this.currentItem, replace);
+        }
     }
 }
