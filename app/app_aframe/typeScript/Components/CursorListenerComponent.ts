2c32e533ce5c79f1920b934822e3536279b5f885
Fabio Sereno <fabio_sereo@hotmail.com>
2018-11-23 16:06:40 +0000

    Starting to look into trigger of animation

--- app/app_aframe/typeScript/Components/CursorListenerComponent.ts
+++ app/app_aframe/typeScript/Components/CursorListenerComponent.ts
@@ -9,6 +9,8 @@ export class CursorListenerComponent implements IComponent {
               this.el.addEventListener('click', function (evt:CustomEvent) {
                     console.log(evt);
                     //lastIndex = (lastIndex + 1) % COLORS.length;
+                    var cameraAnimate = document.querySelector("#cameraMove");
+                    var camera = document.querySelector("#camera");
                     var coords = evt.detail.intersection.point;
                     var offset = 0.70;
                     var x = coords.x;
@@ -16,7 +18,9 @@ export class CursorListenerComponent implements IComponent {
                     var z = coords.z + offset;
                     //this.setAttribute('color', COLORS[lastIndex]);
                     setTimeout(()=>{
-                        document.getElementById("camera").setAttribute("position", x + " " + y + " " + z);
+                        cameraAnimate.setAttribute("to", x + " " + y + " " + z);
+                        cameraAnimate.emit("animateThis");
+                        //camera.setAttribute("position", x + " " + y + " " + z)
                     }, 2000)
               });
             }
