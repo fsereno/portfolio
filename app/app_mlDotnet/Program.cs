9885d1defbb6bc38e18922c3db90b30698ceb862
Fabio Sereno <fabio_sereo@hotmail.com>
2019-03-07 21:06:19 +0000

    Added new application for ML.NET

---
+++ app/app_mlDotnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app_mlDotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
