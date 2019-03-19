7d3c1080e4f3f447e9edc3deff7469fa96886459
Fabio Sereno <fabio_sereo@hotmail.com>
2019-03-19 22:29:44 +0000

    Fixed issue with duplicating reference locally, program running

--- app/app_mlDotnet/Program.cs
+++ app/app_mlDotnet/Program.cs
@@ -13,36 +13,6 @@ namespace myApp
 {
     class Program
     {
-        // STEP 1: Define your data structures
-        // IrisData is used to provide training data, and as
-        // input for prediction operations
-        // - First 4 properties are inputs/features used to predict the label
-        // - Label is what you are predicting, and is only set when training
-        /* public class IrisData
-        {
-            [LoadColumn(0)]
-            public float SepalLength;
-
-            [LoadColumn(1)]
-            public float SepalWidth;
-
-            [LoadColumn(2)]
-            public float PetalLength;
-
-            [LoadColumn(3)]
-            public float PetalWidth;
-
-            [LoadColumn(4)]
-            public string Label;
-        }*/
-
-        // IrisPrediction is the result returned from prediction operations
-       /* public class IrisPrediction
-        {
-            [ColumnName("PredictedLabel")]
-            public string PredictedLabels;
-        }*/
-
         static void Main(string[] args)
         {
             // STEP 2: Create a ML.NET environment  
