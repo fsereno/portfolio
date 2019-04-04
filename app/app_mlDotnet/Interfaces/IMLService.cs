5862bf37b6eb126a8f507dd908901503fff27532
Fabio Sereno <fabio_sereo@hotmail.com>
2019-04-04 22:27:38 +0100

    Interfaces added, Services updated and sln updated. Updated Readme for ml dotnet also

---
+++ app/app_mlDotnet/Interfaces/IMLService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Data.DataView;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+using Entities;
+
+namespace Interfaces
+{
+    public interface IMLService
+    {
+        IDataView Get(string path);
+        Microsoft.ML.Data.EstimatorChain<Microsoft.ML.Transforms.KeyToValueMappingTransformer> Transform();
+        Microsoft.ML.Data.TransformerChain<Microsoft.ML.Transforms.KeyToValueMappingTransformer> Train(Microsoft.ML.Data.EstimatorChain<Microsoft.ML.Transforms.KeyToValueMappingTransformer> pipeline, IDataView trainingDataView); 
+        Entities.IrisPrediction Predict(IrisData irisData, Microsoft.ML.Data.TransformerChain<Microsoft.ML.Transforms.KeyToValueMappingTransformer> model);
+    }
+}
--- app/app_mlDotnet/Services/MLService.cs
+++ app/app_mlDotnet/Services/MLService.cs
@@ -4,46 +4,47 @@ using Microsoft.ML;
 using Microsoft.ML.Data;
 
 using Entities;
+using Interfaces;
 
 namespace Services
 {
-    public class MLService
+    public class MLService : IMLService 
     {
-        public string Predict()
-        {
-            // STEP 2: Create a ML.NET environment  
-            MLContext mlContext = new MLContext();
-
-            // If working in Visual Studio, make sure the 'Copy to Output Directory'
-            // property of iris-data.txt is set to 'Copy always'
-            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<IrisData>(path: "../Data/iris-data.txt", hasHeader: false, separatorChar: ',');
-
-            // STEP 3: Transform your data and add a learner
-            // Assign numeric values to text in the "Label" column, because only
-            // numbers can be processed during model training.
-            // Add a learning algorithm to the pipeline. e.g.(What type of iris is this?)
-            // Convert the Label back into original text (after converting to number in step 3)
-            var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label")
-                .Append(mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
-                .AppendCacheCheckpoint(mlContext)
-                .Append(mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(labelColumnName: "Label", featureColumnName: "Features"))
-                .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
-
-            // STEP 4: Train your model based on the data set  
-            var model = pipeline.Fit(trainingDataView);
-
-            // STEP 5: Use your model to make a prediction
-            // You can change these numbers to test different predictions
-            var prediction = model.CreatePredictionEngine<IrisData, IrisPrediction>(mlContext).Predict(
-                new IrisData()
-                {
-                    SepalLength = 3.3f,
-                    SepalWidth = 1.6f,
-                    PetalLength = 0.2f,
-                    PetalWidth = 5.1f,
-                });
+        private MLContext MlContext;
+        
+        public MLService(MLContext mlContext){
             
-            return prediction.PredictedLabels;
+            MlContext = mlContext;
+        
+        }
+        public IDataView Get(string path){
+            
+            IDataView trainingDataView = MlContext.Data.LoadFromTextFile<IrisData>(path: path, hasHeader: false, separatorChar: ',');
+
+            return trainingDataView;
+        }
+        public Microsoft.ML.Data.EstimatorChain<Microsoft.ML.Transforms.KeyToValueMappingTransformer> Transform(){
+            
+            var pipeline = MlContext.Transforms.Conversion.MapValueToKey("Label")
+                .Append(MlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+                .AppendCacheCheckpoint(MlContext)
+                .Append(MlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(labelColumnName: "Label", featureColumnName: "Features"))
+                .Append(MlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+            
+            return pipeline;
+        }
+        public Microsoft.ML.Data.TransformerChain<Microsoft.ML.Transforms.KeyToValueMappingTransformer> Train(Microsoft.ML.Data.EstimatorChain<Microsoft.ML.Transforms.KeyToValueMappingTransformer> pipeline, IDataView trainingDataView){
+            
+            var model = pipeline.Fit(trainingDataView);
+            
+            return model;
+        }
+        public Entities.IrisPrediction Predict(IrisData irisData, Microsoft.ML.Data.TransformerChain<Microsoft.ML.Transforms.KeyToValueMappingTransformer> model){
+
+            var prediction = model.CreatePredictionEngine<IrisData, IrisPrediction>(MlContext)
+                .Predict(irisData);
+
+            return prediction;
         }
     }
 }
