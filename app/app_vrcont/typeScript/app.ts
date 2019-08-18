5c0f8e08d43aac28c5ece1cf0291a8a4d7f3b3a6
Fabio Sereno <fabio_sereo@hotmail.com>
2019-08-18 22:14:21 +0100

    Started working on basic test threejs scene, with error requestAnimationFrame - it cannot find this

---
+++ app/app_vrcont/typeScript/app.ts
@@ -0,0 +1,12 @@
+
+// Controllers
+import { IndexController } from "./Controllers/IndexController";
+
+// Services
+
+// Instantiate Services with dependency injection
+
+// Controllers
+let indexController = new IndexController();
+
+indexController.init();
