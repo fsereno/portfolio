6175edba744ee5887b90182a510749dcb213b8c7
Fabio Sereno <fabio_sereo@hotmail.com>
2020-03-21 22:19:54 +0000

    EXAMPLES_AUG_19: Added counter increase and limit

--- app/app_toDoVue/src/app.js
+++ app/app_toDoVue/src/app.js
@@ -12,9 +12,10 @@ new Vue({
   },
   methods: {
     addItem: function() {
-      if (this.name.length > 0) {
+      if (this.name.length > 0 && this.counter < this.counterLimit) {
         this.items.push(this.name);
         this.name = "";
+        this.counter = this.counter + 1;
       }
     }
   }
