a7ad084e09a2ebc2169d1ab256e53bfcc57afeb2
Fabio Sereno <fabio_sereo@hotmail.com>
2020-05-06 23:34:22 +0100

    Started to update resources in order to work with latest three js, there are failing frontend tests, unscrabbler is currently failng test

--- app/app_aframe/typeScript/Components/CubikComponent.ts
+++ app/app_aframe/typeScript/Components/CubikComponent.ts
@@ -6,12 +6,12 @@ import { IUpdateService } from "../../../typeScript/Interfaces/IUpdateService";
 import { IRandomGeneratorService } from '../../../typeScript/Interfaces/IRandomGeneratorService';
 
 export class CubikComponent<T extends ICubikModel> implements IComponent<T>, ICubikCollectableComponent {
-    
+
     object: T;
     timerService: ITimerService;
     updateService: IUpdateService;
     randomGeneratorService: IRandomGeneratorService;
-    
+
     constructor(
         object: T,
         timerService: ITimerService,
@@ -23,17 +23,17 @@ export class CubikComponent<T extends ICubikModel> implements IComponent<T>, ICu
         this.updateService = updateService;
         this.randomGeneratorService = randomGeneratorService;
     }
-    
+
     init(): void {
         let self = this;
 
         AFRAME.registerComponent("cubik-component", {
-            
+
             init: function () {
                 self.updateService.update(self.object.scoreId, self.object.player.score);
                 self.updateService.update(self.object.targetId, self.object.getCubeCount());
                 let availableColours = ["green", "red", "blue", "yellow", "purple", "orange"],
-                    cubeClickHandler = (e: CustomEvent) => {
+                    cubeClickHandler = (e: any) => {
                         let cube = e.srcElement,
                             feedbackTextElement = document.querySelector("#"+self.object.feedbackTextElementId);
 
@@ -49,18 +49,18 @@ export class CubikComponent<T extends ICubikModel> implements IComponent<T>, ICu
                                 feedbackTextElement.setAttribute("visible", "true");
                             } else {
                                 feedbackTextElement.setAttribute("visible", "true");
-                            }                                             
+                            }
                         }
                     }
 
-                this.el.addEventListener('click', function (evt:CustomEvent) {
+                this.el.addEventListener('click', function (evt: any) {
 
                     let scene = document.querySelector("#scene"),
                         cube = evt.srcElement,
                         spawnLimit = Number(cube.getAttributeNode("data-spawn-limit").textContent) || 0,
                         timeValue = Number(cube.getAttributeNode("data-time-value").textContent) || 0,
                         spwanAmount = self.randomGeneratorService.Generate(spawnLimit);
-                
+
                     if(cube.classList.contains("start")){
 
                         cube.setAttribute("visible", "false");
@@ -71,14 +71,14 @@ export class CubikComponent<T extends ICubikModel> implements IComponent<T>, ICu
                                 currentPosition = cube.getAttributeNode("position").textContent,
                                 currentPositionArray = currentPosition.split(" "),
                                 newPosition = "",
-                                colourIndex = self.randomGeneratorService.Generate(availableColours.length) - 1;    
-                    
+                                colourIndex = self.randomGeneratorService.Generate(availableColours.length) - 1;
+
                             for(let i = 0; i <cube.attributes.length; i++){
                                 entity.setAttribute(cube.attributes[i].nodeName, cube.attributes[i].nodeValue);
                             }
 
-                            currentPositionArray.forEach(position =>{
-                                let offSet = self.randomGeneratorService.Generate(15);                
+                            currentPositionArray.forEach(() => {
+                                let offSet = self.randomGeneratorService.Generate(15);
                                 newPosition+=offSet+" ";
                             });
 
@@ -89,11 +89,11 @@ export class CubikComponent<T extends ICubikModel> implements IComponent<T>, ICu
                             entity.addEventListener("click", (e: CustomEvent)=>{
                                 cubeClickHandler(e);
                             });
-                            
+
                             scene.appendChild(entity);
                             self.timerService.duration = self.timerService.duration + timeValue;
                         }
-                                        
+
                         self.updateService.update(self.object.scoreId, self.object.player.score);
                         self.updateService.update(self.object.targetId, self.object.getCubeCount());
                         self.timerService.Start();
--- app/app_threeJSScene/src/app.js
+++ app/app_threeJSScene/src/app.js
@@ -1,13 +1,16 @@
 "use strict;"
 
 import * as THREE from 'three';
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.xr.enabled = true;
 document.body.appendChild( renderer.domElement );
+document.body.appendChild( VRButton.createButton( renderer ))
 
 var geometry = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -16,7 +19,13 @@ scene.add( cube );
 
 camera.position.z = 5;
 
-var animate = function () {
+renderer.setAnimationLoop( function() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render( scene, camera );
+});
+
+/*var animate = function () {
   requestAnimationFrame( animate );
 
   cube.rotation.x += 0.01;
@@ -25,6 +34,6 @@ var animate = function () {
   renderer.render( scene, camera );
 };
 
-animate();
+animate();*/
 
 console.log("test");
