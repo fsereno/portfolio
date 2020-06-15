87cdbb5a0130540d41e3638a5b0a3c06401b9aae
Fabio Sereno <fabio_sereo@hotmail.com>
2020-06-15 22:39:27 +0100

    Adding board

--- app/app_ticTacToeReact/src/app.js
+++ app/app_ticTacToeReact/src/app.js
@@ -24,13 +24,26 @@ class Board extends React.Component {
     const status = "Next Player: X";
 
     return (
-
-      // here
-
-    )
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
   }
-
-
 }
 
 ReactDOM.render(
