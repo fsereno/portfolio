42840d6b6e3dff85c89cacef4fab9425cc52b426
Fabio Sereno <fabio_sereo@hotmail.com>
2020-06-27 23:19:55 +0100

    Game now taking turns, and declaring a winner

--- app/app_ticTacToeReact/src/app.js
+++ app/app_ticTacToeReact/src/app.js
@@ -6,30 +6,55 @@ import ReactDOM from 'react-dom';
 //https://reactjs.org/tutorial/tutorial.html
 //https://codepen.io/gaearon/pen/oWWQNa?editors=0010
 
-class Square extends React.Component {
-  render() {
-    return (
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+    return null;
+  }
+}
+
+function Square(props) {
+  return (
     <button
       className="square"
-      onClick={() => this.props.onClick()}>
-      {this.props.value}
+      onClick={props.onClick}>
+        {props.value}
     </button>
-    );
-  }
+  );
 }
 
 class Board extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares});
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext
+      });
   }
 
   renderSquare(i) {
@@ -42,7 +67,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next Player: X";
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ?  "X" : "O");
+    }
     return (
       <div>
         <div className="status">{status}</div>
