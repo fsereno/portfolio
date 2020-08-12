5a04fd8aa0d9c1e6bb6cc27ded8dd85bc1a2c28b
Fabio Sereno <fabio_sereo@hotmail.com>
2020-08-12 20:50:55 +0100

    Making things simpler, need to move on to redux ideas

--- app/app_aframeComplex/src/app.js
+++ app/app_aframeComplex/src/app.js
@@ -7,12 +7,17 @@ class Scene extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: "Hello World!",
-      color: "#4CC3D9",
       cubes: [
-        { position: "-1 0.5 -3" },
-        { position: "0 0.5 -5" },
-        { position: "1 0.5 -3" }
+        {
+          position: "-1 0.5 -3",
+          color: "#4CC3D9"
+        },{
+          position: "0 0.5 -5",
+          color: "#4CC3D9"
+        },{
+          position: "1 0.5 -3",
+          color: "#4CC3D9"
+        }
       ]
     };
   }
@@ -21,32 +26,29 @@ class Scene extends React.Component {
     return `#${Math.floor(Math.random()*16777215).toString(16)}`;
   }
 
-  handleColourChangeClick() {
+  handleColourChangeClick(i) {
+    let cubes = this.state.cubes;
+    let color = this.getRandomColour();
+    if (i <= cubes.length) {
+      cubes[i].color = color;
+    }
     this.setState({
-      color: this.getRandomColour()
+      cubes: cubes
     });
   }
 
   render() {
-    let text = `value: ${this.state.message}; color: #ee1111; height: 13.53; lineHeight: 0.54; width: 2.71; wrapCount: 36.5; xOffset: 1.02; zOffset: 0.51`;
     return (
       <a-scene cursor="rayOrigin: mouse">
-        {this.state.cubes.map((cube) => {
+        {this.state.cubes.map((cube, i) => {
           return(
-            <a-box onClick={() => this.handleColourChangeClick()} position={cube.position} rotation="0 45 0" color={this.state.color} text={text}></a-box>
+            <a-box onClick={() => this.handleColourChangeClick(i)} position={cube.position} rotation="0 45 0" color={cube.color}></a-box>
           );
         })}
         <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
         <a-sky color="#ECECEC"></a-sky>
         <a-entity id="player" position="0.5 0.5 0">
           <a-entity camera="" look-controls="" position="" rotation="" data-aframe-inspector-original-camera="">
-            <a-entity position="-1.2 -0.3 -2">
-              <a-text id="score" position="0.60 0 0" value="0" color="green" width="3"></a-text>
-              <a-text id="target" position="0.60  0.2 0 0" value="0" color="green" width="3"></a-text>
-            </a-entity>
-            <a-entity position="-0.1 -0.3 -2">
-              <a-text id="timer" position="0.40 0 0" value="" color="green" width="3"></a-text>
-            </a-entity>
             <a-entity
               position="0 0 -2"
               geometry="primitive: ring; radiusInner: 0.2; radiusOuter: 0.3;"
