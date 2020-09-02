c2cbe6538ba8de5b2402d085b2b2cb9d3ea31f1e
Fabio Sereno <fabio_sereo@hotmail.com>
2020-09-02 10:51:47 +0100

    Frontend roughly in place, with puzzple to protect aginst bot submits

--- app/app_awsDotNetCoreShoppingCart/src/app.js
+++ app/app_awsDotNetCoreShoppingCart/src/app.js
@@ -4,19 +4,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const API_ENDPOINT = "https://www.something.com";
+const API_SUBMIT_CLASSES_VALID = "btn btn-outline-dark api-submit"
+const API_SUBMIT_CLASSES_NOT_VALID = "btn btn-dark disabled api-submit"
+const ANSWER  = 15;
+
 
 class ShoppingListApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
-      get: API_ENDPOINT
+      get: '',
+      add: '',
+      delete: '',
+      answer: '',
+      isValidBtnClasses: API_SUBMIT_CLASSES_NOT_VALID,
+      isValid: false
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleGetSubmit = this.handleGetSubmit.bind(this);
     this.handleAddSubmit = this.handleAddSubmit.bind(this);
     this.handleDeleteSubmit = this.handleDeleteSubmit.bind(this);
+    this.handlePuzzleSubmit = this.handlePuzzleSubmit.bind(this);
+  }
+
+  open(url) {
+    if (this.state.isValid) {
+      window.open(url, '_blank');
+    }
+  }
+
+  isValidHandler(event) {
+    let isValid = Number(event.target.elements[0].value) === ANSWER;
+    if(isValid) {
+      this.setState({
+        isValid: true,
+        isValidBtnClasses: API_SUBMIT_CLASSES_VALID
+      })
+    }
   }
 
   handleChange(event) {
@@ -25,61 +50,67 @@ class ShoppingListApp extends React.Component {
 
   handleGetSubmit(event) {
     event.preventDefault();
-    window.open('https://usefulangle.com', '_blank');
+    this.open('https://usefulangle.com')
   }
 
   handleAddSubmit(event) {
     event.preventDefault();
-    window.open('https://google.com', '_blank');
+    this.open('https://google.com');
   }
 
   handleDeleteSubmit(event) {
     event.preventDefault();
-    window.open('https://bbc.co.uk', '_blank');
+    this.open('https://bbc.co.uk');
+  }
+
+  handlePuzzleSubmit(event) {
+    event.preventDefault();
+    this.isValidHandler(event);
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleGetSubmit} autoComplete="off">
-          <div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
-            <div class="input-group mr-2">
-              <div class="input-group-prepend">
-                <div class="input-group-text">@</div>
-              </div>
-              <input type="text" class="form-control" placeholder="Input" aria-label="Input" />
-            </div>
-            <div class="btn-group" role="group" aria-label="First group">
-              <button type="submit" class="btn btn-dark api-link">Get</button>
-            </div>
+       <div class="row splitter">
+          <div class="col-lg-4">
+            <form onSubmit={this.handlePuzzleSubmit} autoComplete="off">
+                <label for="basic-url">Anser this to unlock the API</label>
+                <div class="input-group mb-3">
+                  <input required type="text" class="form-control" placeholder="4 x 4 - 1 = ?" aria-label="4 x 4 - 1 = ?" />
+                  <div class="input-group-append">
+                    <button class="btn btn-outline-dark api-submit" type="submit" id="button-addon2">Submit</button>
+                  </div>
+                </div>
+            </form>
+            <form onSubmit={this.handleGetSubmit} autoComplete="off">
+                <label for="basic-url">Leave empty to get all items, or pass an ID</label>
+                <div class="input-group mb-3">
+                  <input type="text" class="form-control" placeholder="Input" aria-label="4 x 4 - 1 = ?" />
+                  <div class="input-group-append">
+                    <button class={this.state.isValidBtnClasses} type="submit" id="button-addon2">Get</button>
+                  </div>
+                </div>
+            </form>
+            <form onSubmit={this.handleAddSubmit} autoComplete="off">
+                <label for="basic-url">label text</label>
+                <div class="input-group mb-3">
+                  <input required type="text" class="form-control" placeholder="input" aria-label="input" />
+                  <div class="input-group-append">
+                    <button class={this.state.isValidBtnClasses} type="submit" id="button-addon2">Add</button>
+                  </div>
+                </div>
+            </form>
+            <form onSubmit={this.handleDeleteSubmit} autoComplete="off">
+                <label for="basic-url">label text</label>
+                <div class="input-group mb-3">
+                  <input required type="text" class="form-control" placeholder="input" aria-label="input" />
+                  <div class="input-group-append">
+                    <button class={this.state.isValidBtnClasses} type="submit" id="button-addon2">Delete</button>
+                  </div>
+                </div>
+            </form>
           </div>
-        </form>
-        <form onSubmit={this.handleAddSubmit} autoComplete="off">
-          <div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
-            <div class="input-group mr-2">
-              <div class="input-group-prepend">
-                <div class="input-group-text">@</div>
-              </div>
-              <input required type="text" class="form-control" placeholder="Input" aria-label="Input" />
-            </div>
-            <div class="btn-group" role="group" aria-label="Second group">
-              <button type="submit" class="btn btn-dark api-link">Add</button>
-            </div>
-          </div>
-        </form>
-        <form onSubmit={this.handleDeleteSubmit} autoComplete="off">
-          <div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
-            <div class="input-group mr-2">
-              <div class="input-group-prepend">
-                <div class="input-group-text">@</div>
-              </div>
-              <input required type="text" class="form-control" placeholder="Input" aria-label="Input" />
-            </div>
-            <div class="btn-group" role="group" aria-label="Third group">
-              <button type="submit" class="btn btn-dark api-link">Delete</button>
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
     );
   }
