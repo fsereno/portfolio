05f922bcd5e27009556407552f533bd9d81a5d7e
Fabio Sereno <fabio_sereo@hotmail.com>
2020-09-11 22:09:24 +0100

    Removing old tests

--- app/app_awsDotNetCoreShoppingCart/aws/test/aws.Tests/BasketControllerTests.cs
+++ app/app_awsDotNetCoreShoppingCart/aws/test/aws.Tests/BasketControllerTests.cs
@@ -155,26 +155,6 @@ namespace aws.Tests
         }
 
         [Fact]
-        public async Task TestDelete()
-        {
-            var lambdaFunction = new LambdaEntryPoint();
-            var requestStr = File.ReadAllText("./SampleRequests/BasketController-Delete.json");
-            var request = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(requestStr);
-            var context = new TestLambdaContext();
-            var response = await lambdaFunction.FunctionHandlerAsync(request, context);
-
-            Assert.Equal(200, response.StatusCode);
-            Assert.Equal("[\"Item 1\"]", response.Body);
-            Assert.True(response.MultiValueHeaders.ContainsKey("Content-Type"));
-            Assert.True(response.MultiValueHeaders.ContainsKey("Access-Control-Allow-Headers"));
-            Assert.True(response.MultiValueHeaders.ContainsKey("Access-Control-Allow-Origin"));
-            Assert.True(response.MultiValueHeaders.ContainsKey("Access-Control-Allow-Methods"));
-            Assert.Equal("Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token", response.MultiValueHeaders["Access-Control-Allow-Headers"][0]);
-            Assert.Equal("*", response.MultiValueHeaders["Access-Control-Allow-Origin"][0]);
-            Assert.Equal("application/json; charset=utf-8", response.MultiValueHeaders["Content-Type"][0]);
-        }
-
-        [Fact]
         public async Task Test_Delete_Post()
         {
             var lambdaFunction = new LambdaEntryPoint();
@@ -225,26 +205,6 @@ namespace aws.Tests
 
             Assert.Equal(200, response.StatusCode);
             Assert.Equal("[]", response.Body);
-            Assert.True(response.MultiValueHeaders.ContainsKey("Content-Type"));
-            Assert.True(response.MultiValueHeaders.ContainsKey("Access-Control-Allow-Headers"));
-            Assert.True(response.MultiValueHeaders.ContainsKey("Access-Control-Allow-Origin"));
-            Assert.True(response.MultiValueHeaders.ContainsKey("Access-Control-Allow-Methods"));
-            Assert.Equal("Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token", response.MultiValueHeaders["Access-Control-Allow-Headers"][0]);
-            Assert.Equal("*", response.MultiValueHeaders["Access-Control-Allow-Origin"][0]);
-            Assert.Equal("application/json; charset=utf-8", response.MultiValueHeaders["Content-Type"][0]);
-        }
-
-        [Fact]
-        public async Task TestDeleteOutOfRange()
-        {
-            var lambdaFunction = new LambdaEntryPoint();
-            var requestStr = File.ReadAllText("./SampleRequests/BasketController-DeleteOutOfRange.json");
-            var request = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(requestStr);
-            var context = new TestLambdaContext();
-            var response = await lambdaFunction.FunctionHandlerAsync(request, context);
-
-            Assert.Equal(200, response.StatusCode);
-            Assert.Equal("[\"Item 1\",\"Item 2\"]", response.Body);
             Assert.True(response.MultiValueHeaders.ContainsKey("Content-Type"));
             Assert.True(response.MultiValueHeaders.ContainsKey("Access-Control-Allow-Headers"));
             Assert.True(response.MultiValueHeaders.ContainsKey("Access-Control-Allow-Origin"));
