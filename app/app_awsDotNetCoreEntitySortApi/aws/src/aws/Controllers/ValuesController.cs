16740af4481654d0998e478d9ee57ab47022e1bd
Fabio Sereno <fabio_sereo@hotmail.com>
2020-09-16 15:50:59 +0100

    Initial post method and test added

--- app/app_awsDotNetCoreEntitySortApi/aws/src/aws/Controllers/ValuesController.cs
+++ app/app_awsDotNetCoreEntitySortApi/aws/src/aws/Controllers/ValuesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using Models;
+
 namespace aws.Controllers
 {
     [Route("api/[controller]")]
@@ -16,29 +18,19 @@ namespace aws.Controllers
             return new string[] { "value1", "value2" };
         }
 
+        [HttpPost]
+        public IList<Employee> GetEmployees()
+        {
+            return new List<Employee>() {
+                new Employee(){ Name = "Joe Bloggs" }
+            };
+        }
+
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
