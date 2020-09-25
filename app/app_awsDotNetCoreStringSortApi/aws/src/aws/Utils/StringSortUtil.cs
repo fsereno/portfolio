1fd291ac0b7269234d89eec07d9239bec5606b07
Fabio Sereno <fabio_sereno@hotmail.com>
2020-09-25 12:25:30 +0100

    Looking at generics and delegates

--- app/app_awsDotNetCoreStringSortApi/aws/src/aws/Utils/StringSortUtil.cs
+++ app/app_awsDotNetCoreStringSortApi/aws/src/aws/Utils/StringSortUtil.cs
@@ -10,8 +10,40 @@ namespace Utils
         public string Sort(string commaSeperatedString)
         {
             var sortedNumeric = this.SortNumeric(commaSeperatedString);
-            var sortedAlpha = this.SortAlpha(commaSeperatedString);
+            //var sortedAlpha = this.SortAlpha(commaSeperatedString);
+            var sortedAlpha = this.SortIt<string>(commaSeperatedString, IsValidString, AddStringToCollection);
+
             var result = $"{sortedNumeric},{sortedAlpha}";
+            return result;
+        }
+
+        public bool IsValidString(string input)
+        {
+            var regex = new Regex("[a-zA-Z]");
+            return regex.IsMatch(input);
+        }
+
+        public bool AddStringToCollection(Tuple<string, List<string>> request)
+        {
+            request.Item2.Add(request.Item1);
+            return true;
+        }
+
+        public string SortIt<T>(string commaSeperatedString, Func<string, bool> method, Func<Tuple<string, List<T>>, bool> addMethod) //AddIfValidString
+        {
+            var characters = commaSeperatedString.Split(',');
+            var charactersToSort = new List<T>();
+
+            foreach (var character in characters)
+            {
+                if (method(character)) // the only problem is tryParse has addtional out par - how to get 2 outs with a Func ?
+                {
+                    addMethod(new Tuple<string, List<T>>(character, charactersToSort));
+                }
+            }
+
+            charactersToSort.Sort();
+            var result = String.Join<T>(',', charactersToSort);
             return result;
         }
 
