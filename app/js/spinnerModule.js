1d484b76dde203453b0658e1dd280241f55c51c8
Fabio Sereno <fabio_sereno@hotmail.com>
2020-10-09 00:15:32 +0100

    Working on spinner, need to move styles central, rough idea is there

--- app/js/spinnerModule.js
+++ app/js/spinnerModule.js
@@ -6,7 +6,16 @@ export let SpinnerModule = function() {
     let Render = function() {
         return(
             <div class="overlay">
-                <div class="loader m-auto mt-10"></div>
+                <div class="item loading-2">
+                    <div class="spinner">
+                        <div class="circle circle-1">
+                            <div class="circle-inner"></div>
+                        </div>
+                        <div class="circle circle-2">
+                            <div class="circle-inner"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
