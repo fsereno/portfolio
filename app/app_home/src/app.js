6a00242a062315892bedef9d04a2f128f3bc702b
Fabio Sereno <fabio_sereno@hotmail.com>
2020-10-13 23:50:05 +0100

    Added collapse to filter, need to have the search and the filter work better together added to theming

--- app/app_home/src/app.js
+++ app/app_home/src/app.js
@@ -53,7 +53,7 @@ class HomeApp extends React.Component {
     this.handleSearch(searchTerm);
   }
 
-  /*handleQuickFilter(event) { 
+  /*handleQuickFilter(event) {
     let searchTerm = event.target.value;
     this.handleSearch(searchTerm);
   }*/
@@ -84,45 +84,52 @@ class HomeApp extends React.Component {
       <div id="contentContainer">
         <form>
           <div id="searchBar" class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Search all available applications..." id="searchInput" onChange={this.handleSearchChange}/>
-            <div class="input-group-append">
+            <div class="input-group-prepend">
               <span class="input-group-text">
                 <i class="fa fa-search"></i>
               </span>
             </div>
+            <input type="text" class="form-control" placeholder="Search applications..." id="searchInput" onChange={this.handleSearchChange}/>
+            <div class="input-group-append">
+              <button class="btn btn-dark" type="button" data-toggle="collapse" data-target="#filterContainer" aria-expanded="false" aria-controls="filterContainer">
+                <i class="fa fa-filter"></i>
+              </button>
+            </div>
           </div>
-          <div class="mb-3">
-            <label class="d-flex flex-row justify-content-center">Quick search</label>
-            <div class="btn-group d-flex flex-row justify-content-center">
-              <button type="button" class="btn btn-outline-dark" value="React" onClick={this.handleSearchChange}>React</button>
-              <button type="button" class="btn btn-outline-dark" value="TypeScript" onClick={this.handleSearchChange}>TypeScript</button>
-              <button type="button" class="btn btn-outline-dark" value=".Net Core" onClick={this.handleSearchChange}>.Net Core</button>
+          <div class="collapse" id="filterContainer">
+            <div class="pb-3">
+              <label class="d-flex flex-row justify-content-center">Quick search</label>
+              <div class="btn-group d-flex flex-row justify-content-center">
+                <button type="button" class="btn btn-outline-dark" value="React" onClick={this.handleSearchChange}>React</button>
+                <button type="button" class="btn btn-outline-dark" value="TypeScript" onClick={this.handleSearchChange}>TypeScript</button>
+                <button type="button" class="btn btn-outline-dark" value=".Net Core" onClick={this.handleSearchChange}>.Net Core</button>
+              </div>
             </div>
           </div>
         </form>
-        <div id="applicationsContainer" class="card-columns">
-          {this.state.applications.map((application) => {
-            if (application.active && application.include) {
-              return (
-                <div class="card p-3 bg-white min-height-160">
-                  <div class="card-body">
-                    <h5 class="card-title">{application.name}</h5>
-                    <p class="card-text">{application.subHeading}</p>
-                    <a class="btn btn-dark mr-2 mb-2"
-                      href={`${Config.prefix}${application.folder}/${Config.index}`}>
-                        View app<i class="fa fa-eye ml-2"></i>
-                    </a>
-                    <a class="btn btn-dark mb-2"
-                      href={`${Config.repoRootUrl}/${Config.folderRoot}${Config.prefix}${application.folder}`}
-                      target="_blank">
-                        View code<i class="fa fa-github-square ml-2"></i>
-                    </a>
+          <div id="applicationsContainer" class="card-columns">
+            {this.state.applications.map((application) => {
+              if (application.active && application.include) {
+                return (
+                  <div class="card p-3 bg-white min-height-160">
+                    <div class="card-body">
+                      <h5 class="card-title">{application.name}</h5>
+                      <p class="card-text">{application.subHeading}</p>
+                      <a class="btn btn-dark mr-2 mb-2"
+                        href={`${Config.prefix}${application.folder}/${Config.index}`}>
+                          View app<i class="fa fa-eye ml-2"></i>
+                      </a>
+                      <a class="btn btn-dark mb-2"
+                        href={`${Config.repoRootUrl}/${Config.folderRoot}${Config.prefix}${application.folder}`}
+                        target="_blank">
+                          View code<i class="fa fa-github-square ml-2"></i>
+                      </a>
+                    </div>
                   </div>
-                </div>
-              )
-            }
-          })}
-        </div>
+                )
+              }
+            })}
+          </div>
       </div>
     )
   }
