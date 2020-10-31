05f6594ee5cb309300070a5f01bc67b14ea500ca
Fabio Sereno <fabio_sereno@hotmail.com>
2020-10-31 00:50:02 +0000

    Working collision detection

--- app/app_home/src/homeThreeModule.js
+++ app/app_home/src/homeThreeModule.js
@@ -10,10 +10,8 @@ export const HomeThreeModule = (async () => {
     let _camera;
     let _renderer;
     let _meshGeometry;
-    let _texture;
     let _meshMaterial;
     let _mesh;
-    let _loader;
 
     let setCameraPosition = () => {
         _camera.position.x = 1;
@@ -36,18 +34,51 @@ export const HomeThreeModule = (async () => {
         });
     }
 
-    let addCube = () => {
-        _mesh.position.x = 1;
-        _mesh.position.y = 1
-        _mesh.position.z = 1
-        _scene.add(_mesh);
+    let createMesh = (x, y, z) => {
+        let geometry = Math.random() + 2;
+        let meshGeometry = new THREE.BoxGeometry(1, 1, 1);
+        let mesh = new THREE.Mesh( meshGeometry, _meshMaterial );
+        mesh.position.x = x || (Math.random() - 0.5) * 10;
+        mesh.position.y = y || (Math.random() - 0.5) * 10;
+        mesh.position.z = z || (Math.random() - 0.5) * 10;
+        return mesh;
+    }
+
+    let addMeshes = (numberOfMeshes = 1) => {
+        for (let i = 0; i < numberOfMeshes; i++) {
+            if (_scene.children.length === 0) {
+                let mesh = createMesh();
+                _scene.add(mesh);
+            } else {
+
+                
+                _scene.traverse(node => {
+                    if (node instanceof THREE.Mesh) {
+
+                        let mesh = createMesh();
+                        
+                        if ( mesh.position.x !== node.position.x
+                            && mesh.position.y !== node.position.y
+                            && mesh.position.z !== node.position.z) {
+
+                             _scene.add(mesh);
+                        }
+                    }
+                });
+            }
+        }
     }
 
     let setAnimationLoop = () => {
         _renderer.setAnimationLoop(function () {
             let time = 0.025;
-            _mesh.rotation.x += time * 0.5;
-            _mesh.rotation.y += time * 0.5;
+
+            _scene.traverse(node => {
+                if (node instanceof THREE.Mesh ) {
+                    node.rotation.x += time * 0.5;
+                    node.rotation.y += time * 0.5;
+                }
+            });
             _renderer.render(_scene, _camera);
         });
     }
@@ -59,15 +90,16 @@ export const HomeThreeModule = (async () => {
         _scene = new THREE.Scene();
         _camera = new THREE.PerspectiveCamera(75, _container.offsetWidth / _container.offsetHeight, 0.1, 1000);
         _renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-        _meshGeometry = new THREE.BoxGeometry(3, 3, 3);
-        _meshMaterial = new THREE.MeshBasicMaterial({ wireframe: true });
-        _mesh = new THREE.Mesh(_meshGeometry, _meshMaterial);
+        //_meshGeometry = new THREE.BoxGeometry(1, 1, 1);
+        _meshMaterial = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x000000});
+        //_mesh = new THREE.Mesh(_meshGeometry, _meshMaterial);
 
         setCameraPosition();
         setRenderer();
         setResizeEventHandler();
-        addCube();
+        addMeshes(3);
         setAnimationLoop();
+
     }
 
     return {
