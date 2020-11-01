2d27d00d3e2cd35f130b4e24653f512a081ad6ef
Fabio Sereno <fabio_sereno@hotmail.com>
2020-11-01 22:42:39 +0000

    working on three js scene

--- app/app_home/src/homeThreeModule.js
+++ app/app_home/src/homeThreeModule.js
@@ -11,7 +11,11 @@ export const HomeThreeModule = (async () => {
     let _renderer;
     let _meshGeometry;
     let _meshMaterial;
-    let _mesh;
+    let _meshGroup;
+    let _raycaster;
+    let _normal;
+    let _relativeVelocity;
+    let _clock;
 
     let setCameraPosition = () => {
         _camera.position.x = 1;
@@ -22,6 +26,8 @@ export const HomeThreeModule = (async () => {
     let setRenderer = () => {
         let container = document.getElementById(_containerId);
         _renderer.setSize(container.offsetWidth, container.offsetHeight);
+        _renderer.shadowMap.enabled = true;
+        _renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         container.appendChild(_renderer.domElement);
     }
 
@@ -34,51 +40,82 @@ export const HomeThreeModule = (async () => {
         });
     }
 
-    let createMesh = (x, y, z) => {
+    let getRandom = (num = 1) => {
+        return - Math.random() * num + Math.random() * num;
+    }
+
+    let createMesh = () => {
         let mesh = new THREE.Mesh( _meshGeometry, _meshMaterial );
-        mesh.position.x = x || (Math.random() - 0.5) * 10;
-        mesh.position.y = y || (Math.random() - 0.5) * 10;
-        mesh.position.z = z || (Math.random() - 0.5) * 10;
+
+        mesh.positionX = getRandom()
+        mesh.positionY = getRandom();
+        mesh.positionZ = getRandom();
+
+        mesh.userData.velocity = new THREE.Vector3();
+        mesh.userData.velocity.x = Math.random() * 0.01 - 0.005;
+        mesh.userData.velocity.y = Math.random() * 0.01 - 0.005;
+        mesh.userData.velocity.z = Math.random() * 0.01 - 0.005;
+
+        var newScaleValue = getRandom(0.3);
+
+        mesh.scale.set(newScaleValue,newScaleValue,newScaleValue);
+        mesh.rotation.x = getRandom(180 * Math.PI / 180);
+        mesh.rotation.y = getRandom(180 * Math.PI / 180);
+        mesh.rotation.z = getRandom(180 * Math.PI / 180);
+        mesh.position.set(mesh.positionX, mesh.positionY, mesh.positionZ);
+
         return mesh;
     }
 
     let addMeshes = (numberOfMeshes = 1) => {
         for (let i = 0; i < numberOfMeshes; i++) {
-            if (_scene.children.length === 0) {
-                let mesh = createMesh();
-                _scene.add(mesh);
-            } else {
-
-                
-                _scene.traverse(node => {
-                    if (node instanceof THREE.Mesh) {
-
-                        let mesh = createMesh();
-                        
-                        if ( mesh.position.x !== node.position.x
-                            && mesh.position.y !== node.position.y
-                            && mesh.position.z !== node.position.z) {
-
-                             _scene.add(mesh);
-                        }
-                    }
-                });
-            }
+            let mesh = createMesh();
+            _meshGroup.add(mesh);
         }
+        _scene.add(_meshGroup);
     }
 
     let setAnimationLoop = () => {
         _renderer.setAnimationLoop(function () {
-            let time = 0.025;
+            let time = performance.now() * 0.0003;
 
-            _scene.traverse(node => {
-                if (node instanceof THREE.Mesh ) {
-                    node.rotation.x += time * 0.5;
-                    node.rotation.y += time * 0.5;
+            for (var i = 0, l = _meshGroup.children.length; i<l; i++) {
+                var mesh = _meshGroup.children[i];
+
+                mesh.rotation.x += 0.008;
+                mesh.rotation.y += 0.005;
+                mesh.rotation.z += 0.003;
+
+                mesh.position.x = Math.sin(time * mesh.positionZ) * mesh.positionY;
+                mesh.position.y = Math.cos(time * mesh.positionX) * mesh.positionZ;
+                mesh.position.z = Math.sin(time * mesh.positionY) * mesh.positionX;
+
+            }
+
+            /*let intersects = _raycaster.intersectObjects(_meshGroup.children)
+        
+            if (intersects.length > 0) {
+                console.log(intersects[0])
+                for (let i = 0; i < intersects.length; i++) {
+
+
+                    //intersects[ i ].object.material.color.set( 0xff0000 );
+
+                    
+                    //intersects[i].position.y = - Math.cos(time * intersects[i].positionX) * mesh.positionZ;
+                    //intersects[i].position.z = - Math.sin(time * intersects[i].positionY) * mesh.positionX;
+
                 }
-            });
+            }*/
+            
             _renderer.render(_scene, _camera);
         });
+    }
+
+    let addLight = (color = 0xFFFFFF, intensity = 1, distance = 1000, x = 0, y = 0, z = 0) => {
+        let light = new THREE.PointLight(color, intensity, distance);
+        light.position.set(x, y, z);
+        _scene.add( light );
     }
 
     let init = () => {
@@ -86,22 +123,26 @@ export const HomeThreeModule = (async () => {
         _containerId = "canvasContainer";
         _container = document.getElementById(_containerId);
         _scene = new THREE.Scene();
-        _camera = new THREE.PerspectiveCamera(75, _container.offsetWidth / _container.offsetHeight, 0.1, 1000);
+        _camera = new THREE.PerspectiveCamera(75, _container.offsetWidth / _container.offsetHeight, 1, 500);
         _renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
         _meshGeometry = new THREE.BoxGeometry(1, 1, 1);
         _meshMaterial = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x000000});
-        //_mesh = new THREE.Mesh(_meshGeometry, _meshMaterial);
+        _meshGroup = new THREE.Object3D();
+        _meshGroup.scale.set(5,5,2)
+        _raycaster = new THREE.Raycaster();
+        _normal = new THREE.Vector3();
+        _relativeVelocity = new THREE.Vector3();
+        _clock = new THREE.Clock();
 
         setCameraPosition();
         setRenderer();
         setResizeEventHandler();
-        addMeshes(5);
+        addMeshes(10);
+        //addLight();
         setAnimationLoop();
-
     }
 
     return {
         init: init
     }
-
 })();
