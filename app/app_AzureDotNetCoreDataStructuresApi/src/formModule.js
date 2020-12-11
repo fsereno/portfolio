8546b89a2a67f4cbc702216709c079eda9c9c510
Fabio Sereno <fabio_sereno@hotmail.com>
2020-12-11 23:26:35 +0000

    Added basic data structures app

---
+++ app/app_AzureDotNetCoreDataStructuresApi/src/formModule.js
@@ -0,0 +1,75 @@
+"use strict;"
+
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+export let FormModule = function() {
+
+  function FormTemplate(props) {
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+
+      const form = event.currentTarget;
+      const data = new FormData(form);
+      const input = data.get("itemInput");
+
+      let isNotUnique = props.items.filter(x => x === input).length > 0;
+
+      if (form.checkValidity() === false || isNotUnique) {
+
+        setValidated(true);
+        event.stopPropagation();
+
+      } else {
+
+        setValidated(false);
+        props.handleSubmit(event);
+
+      }
+
+    };
+
+    return (
+      <>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Row>
+            <Form.Label>
+              Item to add
+            </Form.Label>
+            <InputGroup>
+              <Form.Control
+                name="itemInput"
+                id="itemInput"
+                type="text"
+                placeholder="Add to list..."
+                required
+                onChange={props.onChange}
+                value={props.value}
+              />
+              <InputGroup.Append>
+                <Button id="submit" variant="dark" type="submit">Add item</Button>
+              </InputGroup.Append>
+              <Form.Control.Feedback type="invalid">
+                Please enter a value.
+              </Form.Control.Feedback>
+            </InputGroup>
+          </Form.Row>
+        </Form>
+      </>
+    );
+  }
+
+  let render = function(props) {
+      return (
+          <FormTemplate {...props} />
+      )
+  }
+
+  return {
+    render: render
+  }
+}
