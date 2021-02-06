ea432f9798a5c6ca6b7796a84a43604c287ebdc1
Fabio Sereno <fabio_sereno@hotmail.com>
2021-02-06 21:38:16 +0000

    Using different template

--- app/app_basicSpaReact/Readme.md
+++ app/app_basicSpaReact/Readme.md
@@ -6,6 +6,13 @@ With this application I have built...
 
 The interest in this application...
 
+### Project comands ###
+
+Build JavaScript by running the default Gulp task from witin this directory
+```
+    gulp
+```
+
 Run the application from within the route directory, using the master Gulp file and the default Gulp task.
 ```
     cd ../../
---
+++ app/app_basicSpaReact/src/formComponent.js
@@ -0,0 +1,64 @@
+"use strict;"
+
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { FilterUtil } from '../../typeScript/Utils/filterUtil/dist/index';
+
+export function FormComponent(props) {
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const data = new FormData(form);
+    const input = data.get("itemInput");
+
+    let isNotUnique = !FilterUtil.isUniqueInArray(props.items, input);
+
+    if (form.checkValidity() === false || isNotUnique) {
+
+      setValidated(true);
+      event.stopPropagation();
+
+    } else {
+
+      setValidated(false);
+      props.handleSubmit(event);
+
+    }
+
+  };
+
+  return (
+    <>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Row>
+          <Form.Label>
+            Item to add
+          </Form.Label>
+          <InputGroup>
+            <Form.Control
+              name="itemInput"
+              id="itemInput"
+              type="text"
+              placeholder="Add to list..."
+              required
+              onChange={props.onChange}
+              value={props.value}
+            />
+            <InputGroup.Append>
+              <Button id="submit" variant="dark" type="submit">Add item</Button>
+            </InputGroup.Append>
+            <Form.Control.Feedback type="invalid">
+              Please enter a value.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Row>
+      </Form>
+    </>
+  );
+}
