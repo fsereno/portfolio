01f9cdb816b228fa47fa3d44b67e5f3196896df9
Fabio Sereno <fabio_sereno@hotmail.com>
2021-03-01 13:20:45 +0000

    Looking at app specific tests

--- app/app_basicSpaReact/gulpfile.js
+++ app/app_basicSpaReact/gulpfile.js
@@ -1,5 +1,18 @@
 "use strict";
 const gulp = require("gulp");
 const run = require('gulp-run-command').default;
+const mocha = require("gulp-mocha");
+
+let testsTask = () => {
+    return gulp.src(`./tests/*.test.js`)
+      .pipe(mocha({
+          reporter: "spec",
+      }));
+}
+
+gulp.task("test", (done) => {
+    testsTask();
+    done();
+});
 
 gulp.task('default', run("npx webpack"));
---
+++ app/app_basicSpaReact/tests/util.test.js
@@ -0,0 +1,10 @@
+const chai = require("chai");
+
+describe("Util", () => {
+    describe("Test", () => {
+        it("Should return true", () => {
+            let result = true;
+            chai.expect(result).to.equal(true);
+        });
+    });
+});
