835b34e157e4098af040514ee6f8a03e56d54b60
Fabio Sereno <fabio_sereno@hotmail.com>
2021-04-23 21:30:13 +0100

    Tidy up

--- app/app_basicEmailClientReactSpa/src/components/contextProviders/toasterContextProvider.js
+++ app/app_basicEmailClientReactSpa/src/components/contextProviders/toasterContextProvider.js
@@ -4,19 +4,9 @@
 import React, { useReducer } from 'react';
 import { ToastReducer, ToasterContext } from '../../../../js/modules/react/toaster';
 
-const createToasts = () => {
-    const limit = 0;
-    const toasts = [];
-    for (let i = 0; i < limit; i++ ) {
-      toasts.push({ heading: "Heading", body: "Body" });
-    }
-    return toasts;
-}
 export function ToasterContextProvider({children}) {
 
-    const toasts = createToasts();
-
-    const [ state, dispatch ] = useReducer(ToastReducer, { items: toasts });
+    const [ state, dispatch ] = useReducer(ToastReducer, { items: [] });
 
     const stateValue = { state, dispatch };
 
