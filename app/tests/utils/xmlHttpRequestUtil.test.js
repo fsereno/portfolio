a35db70d548b01d0ec2ce2a58d68e7fd2e327422
Fabio Sereno <fabio_sereno@hotmail.com>
2021-06-05 22:48:21 +0100

    Latest build

--- app/tests/utils/xmlHttpRequestUtil.test.js
+++ app/tests/utils/xmlHttpRequestUtil.test.js
@@ -1,34 +1,34 @@
 "use strict;"
 
 import { expect } from 'chai';
-import { XmlHttpRequestUtil } from '../../js/modules/utils/xmlHttpRequestUtil';
+import { XMLHttpRequestUtil } from '../../js/modules/utils/xmlHttpRequestUtil';
 
 describe("xmlHttpRequestUtil", () => {
     describe("isDone", () => {
         it("Should return true when status is 200 and readyState is 4", () => {
-            const result = XmlHttpRequestUtil.isDone(200, 4);
+            const result = XMLHttpRequestUtil.isDone(200, 4);
             expect(result).to.equal(true);
         });
         it("Should return false when status is not 200 and readyState is 4", () => {
-            const result = XmlHttpRequestUtil.isDone(400, 4);
+            const result = XMLHttpRequestUtil.isDone(400, 4);
             expect(result).to.equal(false);
         });
         it("Should return false when status is 200 but readyState not 4", () => {
-            const result = XmlHttpRequestUtil.isDone(200, 0);
+            const result = XMLHttpRequestUtil.isDone(200, 0);
             expect(result).to.equal(false);
         });
     });
     describe("isFail", () => {
         it("Should return true when status not 200 and readyState is 4", () => {
-            const result = XmlHttpRequestUtil.isFail(0, 4);
+            const result = XMLHttpRequestUtil.isFail(0, 4);
             expect(result).to.equal(true);
         });
         it("Should return false when status is 200 and readyState is 4", () => {
-            const result = XmlHttpRequestUtil.isFail(200, 4);
+            const result = XMLHttpRequestUtil.isFail(200, 4);
             expect(result).to.equal(false);
         });
         it("Should return false when status is not 200 but readyState not 4", () => {
-            const result = XmlHttpRequestUtil.isFail(400, 0);
+            const result = XMLHttpRequestUtil.isFail(400, 0);
             expect(result).to.equal(false);
         });
     });
