b7e2f9e2c50f24c956743f05b209c5e963304e2f
Fabio Sereno <fabio_sereno@hotmail.com>
2021-06-18 23:21:42 +0100

    Some healthcheck updates

--- healthcheck.js
+++ healthcheck.js
@@ -8,18 +8,11 @@ var options = {
     timeout : 2000
 };
 
-var request = http.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    if (res.statusCode == 200) {
-        process.exit(0);
-    }
-    else {
-        process.exit(1);
-    }
-});
+var request = http.request(options, res => res.statusCode == 200
+    ? process.exit(0)
+    : process.exit(1));
 
 request.on('error', function(err) {
-    console.log(err);
     process.exit(1);
 });
 
