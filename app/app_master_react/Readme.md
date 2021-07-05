f515f4e2c253c6805faf17fea017693f5c324a9f
Fabio Sereno <fabio_sereno@hotmail.com>
2021-07-05 15:43:24 +0100

    Renewed the master template example

---
+++ app/app_master_react/Readme.md
@@ -0,0 +1,22 @@
+# To-Do React
+
+With this application I have built a simple To-Do list using React to handle user input and manage state.
+
+### Explanation ###
+
+I have used Webpack to bundle JS dependancies and Babel to transpile to the correct ES version.
+
+The interest in this application is to explore how React can be used for 2 way data binding between the DOM and application state. With a particular interest in Reacts use of a virtual DOM.
+
+### Project commands ###
+
+Build JavaScript by running the default Gulp task from within this directory
+```
+    gulp
+```
+
+Run the application from within the route directory, using the master Gulp file and the default Gulp task
+```
+    cd ../../
+    gulp
+```
---
+++ app/app_master_react/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const webpackUtil = require('../../webpackUtil');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'app.js'),
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'js'),
+  },
+  plugins: [ new MiniCssExtractPlugin() ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: webpackUtil.isProduction(process.env.npm_config_env)
+                ? '[contenthash].[ext]'
+                : '[name].[ext]',
+              outputPath: 'fonts/',
+              publicPath: 'fonts/',
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+    ]
+  }
+};
