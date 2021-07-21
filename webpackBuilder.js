ffc19e0f4176c76968333cd7384fa3fcb1acbff2
Fabio Sereno <fabio_sereno@hotmail.com>
2021-07-21 16:39:52 +0100

    Testing images

--- webpackBuilder.js
+++ webpackBuilder.js
@@ -60,7 +60,7 @@ module.exports = {
 
     const htmlWebpackPluginConfig = new HtmlWebpackPlugin({
       template: "!!pug-loader!" + path.resolve(__dirname, config.developmentDir, `${config.prefix}${application.folder}`, 'pug', 'index.pug'),
-      filename: htmlFilename, //`${path.resolve(__dirname, outputDirectory)}/index.html`
+      filename: htmlFilename,//`${path.resolve(__dirname, 'dist')}/index.html`,
       locals: { config: config, application: application }
     });
 
