bf2d4bf3d5f03bf99419c5d2fd2cc87249ff7730
Fabio Sereno <fabio_sereno@hotmail.com>
2021-07-22 22:35:45 +0100

    Started tidy up of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,11 @@
 
 Usage...
 
-  > gulp default    - To build development resources,
-                      start the dev server and watch for changes.
-                      Navigate to http://localhost:8080 to see the app running.
-
-  > gulp build      - To build development resources and run unit tests on services.
-
-  > gulp frontendTests  - To start development server, run Nightmare tests in a headless browser
-                      and close the server when all tests are done.
+  > gulp frontendTests
+    - To start development server, run Nightmare tests in a headless browser
+    - and close the server when all tests are done.
 
   > gulp test - Run all TypeScript Service unit tests.
-
-  > gulp publish    - To publish production resources to dist
 
   > gulp create     - Update the config.json file with additional applications.
                       Run this command and a new application will
@@ -26,7 +19,6 @@ Usage...
 */
 
 const gulp = require("gulp");
-const pug = require("gulp-pug-3");
 const mocha = require("gulp-mocha");
 const connect = require("gulp-connect");
 const logger = require("gulp-logger");
@@ -36,91 +28,11 @@ const config = require("./config.json");
 const gulpUtil = require("./gulpUtil");
 const flatmap = require("gulp-flatmap");
 
-
 // these can be uninstalled eventually
 const useref = require("gulp-useref");
 const sass = require("gulp-sass");
 const run = require('gulp-run-command').default;
-
-/*let cssTask = (application) => {
-  let directories = gulpUtil.getApplicationDirectories(application);
-  return gulp.src(config.developmentDir+"/"+config.prefix+application.folder+"/sass/styles.scss")
-  .pipe(logger(gulpUtil.populateLoggerOptions(
-      "CSS task started...",
-      "CSS task complete!",
-      ".css",
-      false,
-      "../css",
-      "Compiled to: ",
-      " " + logSymbols.success
-    )))
-    .pipe(sass({outputStyle: 'compressed'}).on("error", sass.logError))
-    .pipe(gulp.dest(directories.applicationDirectory+"/css"))
-    .pipe(connect.reload());
-}*/
-
-const _htmlTask = (application, outputDir) => {
-  return gulp.src(config.developmentDir+"/"+config.prefix+application.folder+"/pug/*.pug")
-  .pipe(logger(gulpUtil.populateLoggerOptions(
-    "HTML task started...",
-    "HTML task complete!",
-    ".html",
-    false,
-    "../",
-    "Compiled to: ",
-    " " + logSymbols.success
-  )))
-  .pipe(pug({
-    pretty: true,
-    locals:{config: config, application: application}
-  }))
-  .pipe(gulp.dest(outputDir))
-  .pipe(connect.reload());
-}
-
-const htmlTask = (application) => {
-  const directories = gulpUtil.getApplicationDirectories(application);
-  return _htmlTask(application, directories.applicationDirectory);
-};
-
-const htmlTaskProd = (application) => {
-  const directories = gulpUtil.getApplicationDirectories(application);
-  return _htmlTask(application, directories.destinationDirectory);
-};
-
-/*let userefTask = (application) => {
-  let directories = gulpUtil.getApplicationDirectories(application);
-  return gulp.src(directories.applicationDirectory+"/*.html")
-  .pipe(logger(gulpUtil.populateLoggerOptions(
-    "Useref task started...",
-    "Useref task complete!",
-    ".html",
-    false,
-    "../../"+directories.destinationDirectory+"/",
-    "Compiled to: ",
-    " " + logSymbols.success
-  )))
-  .pipe(useref())
-  .pipe(gulp.dest(directories.destinationDirectory));
-};*/
-
-// let copyJsTask = (application) => {
-//   let directories = gulpUtil.getApplicationDirectories(application);
-//   if (application.useRequire) {
-//     return gulp.src(config.developmentDir+"/"+config.prefix+application.folder+"/js/**/*.js")
-//       .pipe(logger(gulpUtil.populateLoggerOptions(
-//         "Copy JS task started...",
-//         "Copy JS task complete!",
-//         ".js",
-//         false,
-//         "../../"+directories.destinationDirectory+"/js/",
-//         "Compiled to: ",
-//         " " + logSymbols.success
-//       )))
-//       .pipe(gulp.dest(directories.destinationDirectory+"/js/"));
-//   }
-//   return false;
-// }
+const pug = require("gulp-pug-3");
 
 let createTask = (application) => {
   return directoryExists(config.developmentDir+"/"+config.prefix+application.folder)
@@ -140,17 +52,6 @@ let createTask = (application) => {
         .pipe(gulp.dest(config.developmentDir+"/"+config.prefix+application.folder));
       }
     });
-}
-
-let defaultTasks = (application) => {
-  //gulpUtil.runThis(application, cssTask);
-  gulpUtil.runThis(application, htmlTask);
-}
-
-let publishTasks = (application) => {
-  gulpUtil.runThis(application, htmlTaskProd);
-  //gulpUtil.runThis(application, userefTask);
-  //gulpUtil.runThis(application, copyJsTask);
 }
 
 let createTasks = (application) => {
@@ -207,61 +108,9 @@ let frontendTestTasks = async () => {
   await endServerTask();
 }
 
-// let fontsCopyTask = () => {
-//   return gulp.src(config.developmentDir+"/fonts/**/*")
-//     .pipe(gulp.dest(config.publishDir+"/fonts"));
-// }
-
-// let cssFontsCopyTask = () => {
-//   return gulp.src(config.developmentDir+"/css/fonts/**/*")
-//     .pipe(gulp.dest(config.publishDir+"/css/fonts"));
-// }
-
-let imagesCopyTask = () => {
-  return gulp.src(config.developmentDir+"/images/**/*")
-  .pipe(gulp.dest(config.publishDir+"/images"));
-}
-
-let faviconCopyTask = () => {
-  return gulp.src('app/*.ico')
-    .pipe(gulp.dest(config.publishDir));
-}
-
 let testTask = (directory) => gulpUtil.testTask(`${config.developmentDir}/tests/${directory}`);
 
 let testAllAppsTask = (application) => gulpUtil.testTask(`${config.developmentDir}/${config.prefix}${application.folder}/tests`);
-
-const stopTasks = async () => await endServerTask();
-
-gulp.task("images", (done) => {
-  imagesCopyTask();
-  done();
-});
-
-/*gulp.task("fonts", (done) => {
-  fontsCopyTask();
-  cssFontsCopyTask();
-  done();
-});*/
-
-gulp.task('favicon', (done) => {
-  faviconCopyTask();
-  done();
-})
-
-gulp.task("watch", (done) => {
-  //gulpUtil.watchThis(gulp.watch(config.developmentDir+"/**/sass/*.scss"), "sass", cssTask);
-  gulpUtil.watchThis(gulp.watch(config.developmentDir+"/**/pug/*.pug"), "pug", htmlTask);
-  //gulpUtil.watchThis(gulp.watch(config.developmentDir+"/sass/**/*.scss"), "/", null);
-  //gulpUtil.watchThis(gulp.watch(config.developmentDir+"/pug/**/*.pug"), "/", null);
-  //gulpUtil.watchThis(gulp.watch(config.developmentDir+"/typeScript/**/*.ts"), "/", null);
-  done();
-});
-
-gulp.task("connect", (done) => {
-  startServerTask();
-  done();
-});
 
 gulp.task("test-func", (done) => {
   frontendTestTasks();
@@ -275,31 +124,7 @@ gulp.task("test", (done) => {
   done();
 });
 
-//gulp.task('webpack', run("npm run build"));
-
-//gulp.task('webpack-prod', run("npm run build --env=production"));
-
 gulp.task("create", (done) => {
   config.applications.map(createTasks);
-  done();
-});
-
-gulp.task("publish", gulp.series([/*"test",*/"images", "favicon"], (done) => {
-  config.applications.map(publishTasks);
-  done();
-}));
-
-gulp.task("build", gulp.series(["test"], (done) => {
-  config.applications.map(defaultTasks);
-  done();
-}));
-
-gulp.task("default", gulp.series(["connect", "watch"], (done) => {
-  config.applications.map(defaultTasks);
-  done();
-}));
-
-gulp.task("stop", (done) => {
-  stopTasks();
   done();
 });
