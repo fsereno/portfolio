8c91b229872586a062153f6776f5611825ef109d
Fabio Sereno <fabio_sereno@hotmail.com>
2021-12-09 17:06:23 +0000

    Passing props makes testing much easier with classes, render with error state before acting

--- app/app_toDoReact/tests/toDoList.test.js
+++ app/app_toDoReact/tests/toDoList.test.js
@@ -31,29 +31,39 @@ it("renders", () => {
   expect(container.querySelector('#toDoList')).toBeTruthy();
 });
 
-it("can add an item", () => {
+it("will not add an item with no value", () => {
   act(() => {
     render(<ToDoList />, container);
-    const input = container.querySelector('#itemInput');
-    input.value = 'abc';
     const submitBtn = container.querySelector('#submit');
     submitBtn.dispatchEvent(new MouseEvent('click', { bubbles: true }));
   });
+  expect(container.querySelector('#toDoList').children.length === 0).toBeTruthy();
+});
 
+it("can add an item", () => {
+  act(() => {
+    render(<ToDoList value="abc" />, container);
+    const submitBtn = container.querySelector('#submit');
+    submitBtn.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+  });
   expect(container.querySelector('#toDoList').children.length === 1).toBeTruthy();
 });
 
 it("will not add duplicates", () => {
   act(() => {
-    render(<ToDoList />, container);
-    const input = container.querySelector('#itemInput');
+    render(<ToDoList list={['abc']} value="abc" />, container);
     const submitBtn = container.querySelector('#submit');
-    input.value = 'abc';
     submitBtn.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-
-    // needs work
-
   });
-
   expect(container.querySelector('#toDoList').children.length === 1).toBeTruthy();
+});
+
+it("can delete an item", () => {
+  act(() => {
+    render(<ToDoList list={['abc']} />, container);
+    const item = container.querySelector('#toDoList').children[0];
+    const button = item.querySelector('a');
+    button.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+  });
+  expect(container.querySelector('#toDoList').children.length === 0).toBeTruthy();
 });
