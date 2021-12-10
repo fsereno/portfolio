b842b51dd508e3c620ee4d123ae4f003060788c9
Fabio Sereno <fabio_sereno@hotmail.com>
2021-12-10 22:11:42 +0000

    Removing old tests and tidying up

--- app/app_toDoReact/tests/toDoList.test.js
+++ app/app_toDoReact/tests/toDoList.test.js
@@ -3,65 +3,52 @@
  */
 
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import { mount } from 'enzyme';
 import ToDoList from '../src/toDoList';
 
-let container = null;
-
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
+beforeEach(() => {});
+afterEach(() => {});
 
 it("renders", () => {
-  act(() => {
-    render(<ToDoList />, container);
-  });
-  expect(container.querySelector('#toDoList')).toBeTruthy();
+  const wrapper = mount(<ToDoList />);
+  expect(wrapper.find('#toDoList')).toBeTruthy();
 });
 
 it("will not add an item with no value", () => {
-  act(() => {
-    render(<ToDoList />, container);
-    const submitBtn = container.querySelector('#submit');
-    submitBtn.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-  });
-  expect(container.querySelector('#toDoList').children.length === 0).toBeTruthy();
+  const wrapper = mount(<ToDoList />);
+  const form = wrapper.find('form');
+  form.simulate('submit');
+  expect(wrapper.find('ul#toDoList').children()).toHaveLength(0);
 });
 
 it("can add an item", () => {
-  act(() => {
-    render(<ToDoList value="abc" />, container);
-    const submitBtn = container.querySelector('#submit');
-    submitBtn.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-  });
-  expect(container.querySelector('#toDoList').children.length === 1).toBeTruthy();
+  const wrapper = mount(<ToDoList />);
+  const input = wrapper.find('input#itemInput');
+  const form = wrapper.find('form');
+  input.simulate('change', { target: { value: 'abc' } });
+  form.simulate('submit');
+  expect(wrapper.find('ul#toDoList').children()).toHaveLength(1);
 });
 
-it("will not add duplicates", () => {
-  act(() => {
-    render(<ToDoList list={['abc']} value="abc" />, container);
-    const submitBtn = container.querySelector('#submit');
-    submitBtn.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-  });
-  expect(container.querySelector('#toDoList').children.length === 1).toBeTruthy();
+it("will not add a duplicate", () => {
+  const wrapper = mount(<ToDoList />);
+  const input = wrapper.find('input#itemInput');
+  const form = wrapper.find('form');
+  input.simulate('change', { target: { value: 'abc' } });
+  form.simulate('submit');
+  input.simulate('change', { target: { value: 'abc' } });
+  form.simulate('submit');
+  expect(wrapper.find('ul#toDoList').children()).toHaveLength(1);
 });
 
 it("can delete an item", () => {
-  act(() => {
-    render(<ToDoList list={['abc']} />, container);
-    const item = container.querySelector('#toDoList').children[0];
-    const button = item.querySelector('a');
-    button.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-  });
-  expect(container.querySelector('#toDoList').children.length === 0).toBeTruthy();
+  const wrapper = mount(<ToDoList />);
+  const input = wrapper.find('input#itemInput');
+  const form = wrapper.find('form');
+  input.simulate('change', { target: { value: 'abc' } });
+  form.simulate('submit');
+  const item = wrapper.find('ul#toDoList').childAt(0);
+  const deleteBtn = item.find('a.delete');
+  deleteBtn.simulate('click');
+  expect(wrapper.find('ul#toDoList').children()).toHaveLength(0);
 });
