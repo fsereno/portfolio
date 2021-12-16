439876448e73e8770df71faddbaf0fb0e060b441
Fabio Sereno <fabio_sereno@hotmail.com>
2021-12-16 17:39:21 +0000

    There is no need to test the router, so just render the pages direct and test on their output

---
+++ app/app_basicEmailClientReactSpa/tests/home.test.js
@@ -0,0 +1,50 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { mount } from 'enzyme';
+import { ToasterContextProvider } from "../../js/modules/react/toasterComponent";
+import { EmailContextProvider } from "../src/components/contextProviders/emailContextProvider";
+import { ContentContainer } from '../src/components/contentContainer.js'
+import { Home } from "../src/components/pages/home";
+
+jest.mock('../src/data/bodies.js', () => {
+    return function getBodies() {
+        return [
+            "This is a body text.",
+            "Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+            "Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC"
+        ];
+    }
+});
+
+jest.mock('../src/data/emailAddresses.js', () => {
+    return function getEmailAddresses() {
+        return [
+            "test@test.co.uk",
+            "bob@jones.co.uk",
+            "joe@bloggs.co.uk",
+            "tom@jones.co.uk",
+        ]
+    };
+});
+
+beforeEach(() => { });
+afterEach(() => { });
+
+const App = () => {
+    return (
+        <ToasterContextProvider>
+            <EmailContextProvider>
+                <Home />
+            </EmailContextProvider>
+        </ToasterContextProvider>
+    )
+}
+
+it("can render", () => {
+    const wrapper = mount(<App />);
+    console.log(wrapper.debug());
+    expect(wrapper.find(<ContentContainer />)).toBeTruthy();
+});
