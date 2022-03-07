14a3b30be8252129ac121ef4bc10a4d20fdeebef
Fabio Sereno <fabio_sereno@hotmail.com>
2022-03-07 22:25:42 +0000

    Started looking at puppeteer to handle end to end instead of nightmare

---
+++ app/app_awsDotNetCoreAsyncCoffeeMachine/tests/coffeeMakerComponent-pup.test.js
@@ -0,0 +1,37 @@
+import puppeteer from 'puppeteer';
+
+//create global variables to be used in the beforeAll function
+let browser
+let page
+
+beforeAll(async () => {
+  // launch browser
+  browser = await puppeteer.launch(
+    {
+      headless: true, // headless mode set to false so browser opens up with visual feedback
+      slowMo: 250, // how slow actions should be
+      args: ['--no-sandbox']
+    }
+  )
+  // creates a new page in the opened browser
+  page = await browser.newPage()
+})
+
+describe('app_awsDotNetCoreAsyncCoffeeMachine', () => {
+  test('The Puzzle Modal is rendered', async () => {
+    await page.goto('http://localhost:8080/app_awsDotNetCoreAsyncCoffeeMachine/index.html');
+    await page.waitForSelector('#puzzleModal');
+  }, 16000);
+  test('Solve puzzle correctly', async () => {
+    await page.goto('http://localhost:8080/app_awsDotNetCoreAsyncCoffeeMachine/index.html');
+    await page.waitForSelector('#puzzleModal');
+    await page.type('#puzzleModal input[type=text]', '5');
+    await page.click('#submitPuzzle');
+    await page.waitForSelector('#puzzleModal', { hidden: true });
+  }, 16000);
+});
+
+// This function occurs after the result of each tests, it closes the browser
+afterAll(() => {
+  browser.close()
+})
