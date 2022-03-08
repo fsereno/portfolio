e3dd0b017d620d7428562f3e9ff2ef0aa1f3008f
Fabio Sereno <fabio_sereno@hotmail.com>
2022-03-08 22:59:22 +0000

    Looks like from the logs the endpoint is hit, bt cannot eval the result in the test

--- app/app_awsDotNetCoreAsyncCoffeeMachine/tests/coffeeMakerComponent-pup.test.js
+++ app/app_awsDotNetCoreAsyncCoffeeMachine/tests/coffeeMakerComponent-pup.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import puppeteer from 'puppeteer';
 
 //create global variables to be used in the beforeAll function
@@ -21,14 +25,23 @@ describe('app_awsDotNetCoreAsyncCoffeeMachine', () => {
   test('The Puzzle Modal is rendered', async () => {
     await page.goto('http://localhost:8080/app_awsDotNetCoreAsyncCoffeeMachine/index.html');
     await page.waitForSelector('#puzzleModal');
-  }, 16000);
-  test('Solve puzzle correctly', async () => {
+  }, 10000);
+  test('Solve puzzle correctly and close the Puzzle modal', async () => {
     await page.goto('http://localhost:8080/app_awsDotNetCoreAsyncCoffeeMachine/index.html');
     await page.waitForSelector('#puzzleModal');
     await page.type('#puzzleModal input[type=text]', '5');
     await page.click('#submitPuzzle');
     await page.waitForSelector('#puzzleModal', { hidden: true });
-  }, 16000);
+  }, 10000);
+  test('Should run the process synchronously', async () => {
+    await page.goto('http://localhost:8080/app_awsDotNetCoreAsyncCoffeeMachine/index.html');
+    await page.waitForSelector('#puzzleModal');
+    await page.type('#puzzleModal input[type=text]', '5');
+    await page.click('#submitPuzzle');
+    await page.waitForSelector('#puzzleModal', { hidden: true });
+    await page.click('#runSync')
+    // hmm dont think this is hitting the outside world.
+  }, 10000);
 });
 
 // This function occurs after the result of each tests, it closes the browser
