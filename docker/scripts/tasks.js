d3bef552a90c41e29420b3c63ee46d434618a8d5
Fabio Sereno <fabio_sereno@hotmail.com>
2024-05-01 21:40:27 +0100

    Task has been rolled into start with remove flag

--- docker/scripts/tasks.js
+++
@@ -1,8 +0,0 @@
-const handlers = require('./handlers.common');
-const tasksHandlers = require('./handlers.tasks');
-const verbs = require('./verbs.tasks');
-const constants = require('./constants.tasks');
-
-handlers.ifHasUnsupported(verbs.supported);
-handlers.ifHasHelp(verbs.hasHelp, constants.help);
-tasksHandlers.runIfHasName();
